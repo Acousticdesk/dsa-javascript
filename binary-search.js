af236b02759dc6a121ea2fb4ac9bc437be0fff57
Andrii Kicha <akicha@star.global>
2023-03-03 19:20:57 +0100

    add iterative binary search algorithm

--- binary-search.js
+++ binary-search.js
@@ -1,6 +1,6 @@
 function binarySearch(sortedArr, value, removedLength = 0) {
-  if (sortedArr.length === 0) {
-    return -1;
+  if (sortedArr.length === 1) {
+    return sortedArr[0] === value ? removedLength : -1;
   }
 
   const midpoint = Math.floor(sortedArr.length / 2);
@@ -22,6 +22,28 @@ function binarySearch(sortedArr, value, removedLength = 0) {
   return binarySearch(subArray, value, totalRemovedLength);
 }
 
+function binarySearchIterative(sortedArr) {
+  let first = 0;
+  let last = sortedArr.length - 1;
+
+  while (first <= last) {
+    const midpoint = Math.floor((last - first) / 2);
+
+    if (sortedArr[midpoint] === value) {
+      return midpoint;
+    }
+
+    if (value < sortedArr[midpoint]) {
+      last = midpoint - 1;
+    } else {
+      first = midpoint + 1;
+    }
+  }
+
+  return -1;
+}
+
 module.exports = {
   binarySearch,
+  binarySearchIterative,
 };
--- binary-search.spec.js
+++ binary-search.spec.js
@@ -1,6 +1,6 @@
-const { binarySearch } = require("./binary-search");
+const { binarySearch, binarySearchIterative } = require("./binary-search");
 
-describe("given binary-search", () => {
+describe("given binarySearch", () => {
   it("should search for an item in a sorted array", () => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -11,5 +11,31 @@ describe("given binary-search", () => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     expect(binarySearch(arr, 1)).toEqual(0);
+  });
+
+  it("should return 0 if no items were found", () => {
+    const arr = [1, 2, 3];
+
+    expect(binarySearch(arr, 4)).toEqual(-1);
+  });
+});
+
+describe("given binarySearchIterative", () => {
+  it("should search for an item in a sorted array", () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    expect(binarySearch(arr, 9)).toEqual(arr.length - 2);
+  });
+
+  it("should find a value and return a correct index from the beginning of the array", () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    expect(binarySearch(arr, 1)).toEqual(0);
+  });
+
+  it("should return 0 if no items were found", () => {
+    const arr = [1, 2, 3];
+
+    expect(binarySearch(arr, 4)).toEqual(-1);
   });
 });
